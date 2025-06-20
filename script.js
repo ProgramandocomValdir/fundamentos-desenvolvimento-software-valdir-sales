1a247c7456ac7aad3fc6c8a4836b5d0c77ac507d
ProgramandocomValdir <programandocomvaldir@outlook.com>
2025-06-20 17:41:31 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -2,4 +2,5 @@ function mostrarMensagem() {
     const mensagem = "Olá, seja bem-vindo(a)! Que Deus te abençoe sempre!";
     const div = document.getElementById("mensagem");
     div.textContent = mensagem;
+    
 }
